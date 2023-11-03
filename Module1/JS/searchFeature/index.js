7fd19e56618bc3b4e8a0f988419ee06cc80b0d8b
huudongg25 <thdong98225@gmail.com>
2023-11-03 14:13:03 +0700

    update

--- Module1/JS/searchFeature/index.js
+++ Module1/JS/searchFeature/index.js
@@ -1,19 +1,44 @@
-const products = ["iphone","macbook","ipod","ipad","applewatch"]
+const products = [{
+    name: "iphone",
+    price: 400,
+    desc: "lorem...",
+},
+{
+    name: "ipod",
+    price: 300,
+    desc: "lorem..."
+},
+{
+    name: "ipad",
+    price: 200,
+    desc: "lorem..."
+},
+{
+    name: "mac",
+    price: 200,
+    desc: "lorem..."
+}]
+
+
 
 const list = document.getElementById("list")
 
-products.forEach(function(element,index){
-    list.innerHTML += `<li>${element}</li>`
+products.forEach(function (element, index) {
+    list.innerHTML += `<div>
+        <span>${element.name}</span>
+       ${element.price == 400 ? `<span>${element.price}</span>` : `<span>Không thích</span>`}
+        <span>${element.desc}</span>
+    </div>`
 })
 
-function search(){
+function search() {
     const input = prompt("Nhập vào sản phẩm")
-    const searchProducts = products.filter(function(element,index){
+    const searchProducts = products.filter(function (element, index) {
         return element.includes(input)
     })
-    
+
     list.innerHTML = ""
-    searchProducts.forEach(function(element,index){
+    searchProducts.forEach(function (element, index) {
         list.innerHTML += `<li>${element}</li>`
     })
 }
