a6070aed81fe634af97a384ee72ee310f282afa3
huudongg25 <thdong98225@gmail.com>
2023-11-14 09:47:58 +0700

    update

---
+++ Module1/JS/demoImportExport/export.js
@@ -0,0 +1,11 @@
+export default function notify() {
+   return alert('ok')
+}
+
+export function test(){
+
+}
+
+export function test2(){
+   
+}
---
+++ Module1/JS/demoImportExport/import.js
@@ -0,0 +1,4 @@
+import notify from "./export.js";
+import { test, test2 } from './export.js'
+
+notify()
