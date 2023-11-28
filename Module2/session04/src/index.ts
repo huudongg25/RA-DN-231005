efcda35e27802fa599e25098c9d81a1fb86c3f0d
huudongg25 <thdong98225@gmail.com>
2023-11-28 16:34:02 +0700

    update

--- Module2/session04/src/index.ts
+++ Module2/session04/src/index.ts
@@ -1,1 +1,57 @@
-console.log(1111);
+class Parents {
+    fullName: string
+    age: number
+    constructor(a: string, b: number) {
+        this.fullName = a
+        this.age = b
+    }
+
+    getFullName(): string {
+        return this.fullName
+    }
+}
+
+class Child2 extends Parents {
+    hobby: string
+    constructor(hobby: string, fullName: string, age: number) {
+        super(fullName, age)
+        this.hobby = hobby
+    }
+
+    getInfor(): void {
+        console.log(super.getFullName());
+        console.log(this.hobby);
+        console.log(this.age);
+    }
+}
+
+class Child1 extends Parents {
+    birth: string
+    constructor(birth: string, fullName: string, age: number) {
+        super(fullName, age)
+        this.birth = birth
+    }
+
+    override getFullName(): string {
+        return "Tên gì kệ tao"
+    }
+
+    getInfor(): void {
+        console.log(super.getFullName());
+        console.log(this.birth);
+        console.log(this.age);
+    }
+}
+
+class Chau extends Child1 {
+    gender: string
+    constructor(gender: string, birth: string, fullName: string, age: number) {
+        super(birth, fullName, age)
+        this.gender = gender
+    }
+
+}
+
+const chau = new Chau("Nữ","1/1/2000", "Phúc", 20)
+
+console.log(chau.getFullName());
---
+++ Module2/session04/src/test.ts
@@ -0,0 +1,13 @@
+class Person {
+    name:string
+    constructor(name:string) {
+        this.name = name
+    }
+}
+
+function sum():string{
+    console.log('ok');
+    return 'ok'
+}
+
+console.log(sum());
