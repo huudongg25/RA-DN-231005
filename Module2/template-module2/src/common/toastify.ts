8a138909f4c3d49ae78a06a666350da790e9d74b
huudongg25 <thdong98225@gmail.com>
2023-12-21 13:44:20 +0700

    update

---
+++ Module2/template-module2/src/common/toastify.ts
@@ -0,0 +1,27 @@
+import { toast } from "react-toastify";
+
+export function notifySucces(msg: string) {
+    toast.success(msg, {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+    })
+}
+
+export function notifyError(msg: string) {
+    toast.error(msg, {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+    })
+}
