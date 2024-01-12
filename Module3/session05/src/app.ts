6780856687145f8f8b5c5b28e1a5688d676fcfef
huudongg25 <thdong98225@gmail.com>
2024-01-12 14:51:49 +0700

    update

---
+++ Module3/session05/src/app.ts
@@ -0,0 +1,28 @@
+import express from 'express'
+import path from 'path';
+
+const app = express()
+app.use(express.static('public'));
+app.set("view engine", "ejs") //set toàn bộ dự án sử dụng ejs
+app.set("views", path.join(__dirname, "views")); // cấu hình file lưu trữ template engine
+
+app.get('/', (req, res) => {
+    res.render('home', {
+        username: "Phúc",
+        list: [{ id: 1, name: 'hieu' }, { id: 2, name: 'hieu2' }],
+    })
+})
+
+app.get('/test', (req, res) => {
+    res.render('test')
+})
+app.delete('/user/:id', (req, res) => {
+    //logic xoá
+    console.log(req.params.id);
+    res.json('đã xoá')
+})
+
+
+app.listen(8000, () => {
+    console.log(`http://localhost:8000`);
+})
