67dac5c355fd88ab5fae8acdcc573c4e669220f8
huudongg25 <thdong98225@gmail.com>
2024-01-30 09:45:49 +0700

    update

--- Module3/praticeMulterUser/src/app.ts
+++ Module3/praticeMulterUser/src/app.ts
@@ -3,9 +3,18 @@ import * as dotenv from 'dotenv'
 import sequelize from './configs/db.config'
 import createTable from './entities'
 import Router from './router'
+import cookieParser from 'cookie-parser'
+import session from 'express-session'
 dotenv.config()
 const app = express()
 const PORT = process.env.PORT
+app.use(cookieParser())
+app.use(session({
+    secret: 'Dong',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
+}))
 app.use(urlencoded())
 
 sequelize.authenticate()
--- Module3/praticeMulterUser/src/controllers/usersController.ts
+++ Module3/praticeMulterUser/src/controllers/usersController.ts
@@ -24,12 +24,13 @@ usersController
             res.status(400).json({ msg: 'create failed' })
         }
     })
-    .post('/login', async (req, res) => {
+    .post('/login', async (req: any, res: Response) => {
         try {
             const loginForm = {
                 email: req.body.email,
                 password: req.body.password
             }
+            req.session.test = 123;
             const result = await userService.login(loginForm)
             if (result == 1) {
                 res.status(400).json({ msg: 'Email wrong' })
@@ -63,12 +64,39 @@ usersController
             res.status(400).json({ msg: 'update failed' })
         }
     })
-    .get('/get-all', authMiddleware, async (req, res) => {
+    .get('/get-all', authMiddleware, async (req: any, res) => {
         try {
             const data = await userService.getAll()
             res.status(200).json({ data })
         } catch (error) {
             res.status(400).json({ msg: 'get failed' })
+        }
+    })
+    .get('/create-otp', async (req: any, res) => {
+        try {
+            const random = Math.ceil(Math.random() * 10000)
+            var salt = bcrypt.genSaltSync(10); 
+            var hashRandom = bcrypt.hashSync(String(random), salt);
+            res.cookie('otp', hashRandom, {expires: new Date(Date.now() + 120000), httpOnly: true })
+            res.status(200).json(random)
+        } catch (error) {
+            console.log(error);
+            res.status(400).json({ msg: 'get failed' })
+        }
+    })
+    .post('/confirm-otp', async (req: any, res) => {
+        try {
+            const otpCookie = req.cookies.otp;
+            const bodyOtp = req.body.otp
+            console.log(otpCookie);
+            const checkOTP = bcrypt.compareSync(bodyOtp,otpCookie)
+            if(checkOTP) {
+                res.status(200).json('confirm successfully')
+            }else {
+                res.status(400).json('confirm failed')
+            }
+        } catch (error) {
+            res.status(400).json({ msg: 'time out otp' })
         }
     })
 
